39ff93cc621a4279008b758b08e1a6406f12b9fb
vasily <sva7777@gmail.com>
2022-07-19 11:41:56 +0300

    resolve task_18_4

--- exercises/18_using_asyncio/task_18_4.py
+++ exercises/18_using_asyncio/task_18_4.py
@@ -24,3 +24,117 @@
 
 Для заданий в этом разделе нет тестов!
 '''
+
+
+import base_ssh_class
+import yaml
+import asyncio
+from pprint import pprint
+
+
+class CiscoSSH (base_ssh_class.BaseSSH):
+    
+    def __init__(self, host, username, password, secret, timeout=3):
+        super().__init__(host, username, password, timeout)
+        self.secret = secret
+    async def connect(self):
+        await super().connect()
+
+        self._writer.write("enable\n")
+        await asyncio.wait_for(self._reader.readuntil("Password"), self.timeout)
+        self._writer.write(f"{self.secret}\n")
+        await asyncio.wait_for(self._reader.readuntil([">", "#"]), self.timeout)
+        self._writer.write("terminal length 0\n")
+        await asyncio.wait_for(self._reader.readuntil("#"), self.timeout)
+
+        return self
+    
+    async def send_show_command(self, command):
+        result = ""
+
+        self._writer.write(command+"\n")
+        result = result + await asyncio.wait_for(self._reader.readuntil("#"), self.timeout)
+        
+        return result
+    
+    async def send_config_commands(self, commands):
+        result = ""
+        if type(commands)== str:
+            commands = commands[commands]
+
+        self._writer.write("conf t\n")
+        result = result + await asyncio.wait_for(self._reader.readuntil("#"), self.timeout)
+
+        for command in commands:
+            self._writer.write(command+"\n")
+            result = result + await asyncio.wait_for(self._reader.readuntil("#"), self.timeout)
+        
+        self._writer.write("exit\n")
+        result = result + await asyncio.wait_for(self._reader.readuntil("#"), self.timeout)
+        
+        return result
+        
+    async def send_config_set(self, commands):
+        return await self.send_config_commands(commands)
+
+
+async def show_command_task(device, command):
+    device.pop("device_type", None)
+    
+    async with CiscoSSH(**device) as cisco:
+        result = await cisco.send_show_command(command)
+    return result
+    
+
+async def config_commands_task(device, commands):
+    device.pop("device_type", None)
+    
+    async with CiscoSSH(**device) as cisco:
+        result = await cisco.send_config_commands(commands)
+    return result
+
+async def config_set_task(device, commands):
+    device.pop("device_type", None)
+    
+    async with CiscoSSH(**device) as cisco:
+        result = await cisco.send_config_set(commands)
+    return result
+
+
+async def show_command_main(devices, command):
+    tasks = [asyncio.ensure_future(show_command_task(dev, command) ) for dev in devices]
+    result = await asyncio.gather(*tasks)
+    return result
+    
+async def config_commands_main(devices, commands):
+    tasks = [asyncio.ensure_future(config_commands_task(dev, commands) ) for dev in devices]
+    result = await asyncio.gather(*tasks)
+    return result
+
+async def config_set_main(devices, commands):
+    tasks = [asyncio.ensure_future(config_set_task(dev, commands) ) for dev in devices]
+    result = await asyncio.gather(*tasks)
+    return result
+
+if __name__ == "__main__":
+
+    with open("devices.yaml") as f:
+        devices_all = yaml.safe_load(f)
+
+    devices =[]
+    
+    for dev in devices_all:
+        if "device_type" in dev and dev["device_type"] == "cisco_ios":
+            devices.append(dev)
+
+    loop = asyncio.get_event_loop()
+    
+    pprint(loop.run_until_complete(show_command_main(devices,"sh clock"))) 
+    
+    commands = ['interface loopback55', 'ip address 10.5.5.5 255.255.255.255']
+    
+    pprint(loop.run_until_complete(config_commands_main(devices,commands))) 
+    
+    pprint(loop.run_until_complete(config_set_main(devices,commands))) 
+    
+    
